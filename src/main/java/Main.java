2925ffd0c1073f4a6d04b75dc837842f28f3bf94
Arthur Kalule <kalulearthur@gmail.com>
2021-07-09 14:42:04 +0300

    update hibernate configuration and session factory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
 public class Main
 {
+    // re-usable reference to a session Factory
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory()
+    {
+        // create a standard registry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    }
+
     public static void main(String[] args){
         System.out.println("Hello Hibernate");
     }
